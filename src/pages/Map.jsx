82c2c72f3fa48ef510b2b207030d3de4d1ef695f
Fionn Hourican <fionn.hourican2@mail.dcu.ie>
2025-10-06 15:42:51 +0100

    Add filter icon to map page

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
-import { Typography, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { Typography, Box, IconButton, Menu, MenuItem, TextField, Stack } from '@mui/material';
+import FilterListIcon from '@mui/icons-material/FilterList';
 import TableauReact from '../components/TableauReact';
 
 export default function Map() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [minAge, setMinAge] = useState('');
+  const [maxAge, setMaxAge] = useState('');
+
+  const handleFilterClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ p: 2 }}>
-      <Typography variant="h4" gutterBottom>
-        Irish speakers in longford
-      </Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 2 }}>
+        <Typography variant="h4">
+          Irish speakers in longford
+        </Typography>
+        <IconButton onClick={handleFilterClick}>
+          <FilterListIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem>
+            <Stack spacing={2} sx={{ p: 1, minWidth: 200 }}>
+              <TextField
+                label="Min Age"
+                type="number"
+                size="small"
+                value={minAge}
+                onChange={(e) => setMinAge(e.target.value)}
+              />
+              <TextField
+                label="Max Age"
+                type="number"
+                size="small"
+                value={maxAge}
+                onChange={(e) => setMaxAge(e.target.value)}
+              />
+            </Stack>
+          </MenuItem>
+        </Menu>
+      </Box>
       <TableauReact />
     </Box>
   );
