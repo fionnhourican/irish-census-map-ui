8435cdd93bafe9a076d91370a0195ba779b08003
Fionn Hourican <fionn.hourican2@mail.dcu.ie>
2025-10-07 21:14:16 +0100

    Add filter options to filter button
    These are not set up and just in the UI for now. Must be implemented in
    te backend api first.

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,14 +1,31 @@
 import React, { useState, useCallback } from 'react';
-import { Typography, Box, IconButton, Menu, MenuItem, TextField, Stack } from '@mui/material';
+import { Typography, Box, IconButton, Menu, MenuItem, TextField, Stack, Button, Select, FormControl, InputLabel } from '@mui/material';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import D3MapVisualization from '../components/D3MapVisualization';
 import TownlandDetailsPanel from '../components/TownlandDetailsPanel';
 
 export default function Map() {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [minAge, setMinAge] = useState('');
-  const [maxAge, setMaxAge] = useState('');
   const [selectedTownland, setSelectedTownland] = useState(null);
+  const [showMoreFilters, setShowMoreFilters] = useState(false);
+  
+  // Basic filters
+  const [surname, setSurname] = useState('');
+  const [forename, setForename] = useState('');
+  const [age, setAge] = useState('');
+  
+  // Advanced filters
+  const [sex, setSex] = useState('');
+  const [occupation, setOccupation] = useState('');
+  const [countyOrigin, setCountyOrigin] = useState('');
+  const [religion, setReligion] = useState('');
+  const [literacy, setLiteracy] = useState('');
+  const [maritalStatus, setMaritalStatus] = useState('');
+  const [relationship, setRelationship] = useState('');
+  const [specifiedIllnesses, setSpecifiedIllnesses] = useState('');
+  const [yearsMarried, setYearsMarried] = useState('');
+  const [childrenBorn, setChildrenBorn] = useState('');
+  const [childrenLiving, setChildrenLiving] = useState('');
 
   const handleFilterClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -40,23 +57,193 @@ export default function Map() {
           anchorEl={anchorEl}
           open={Boolean(anchorEl)}
           onClose={handleClose}
+          PaperProps={{ sx: { maxHeight: '80vh', width: 350 } }}
         >
           <MenuItem>
-            <Stack spacing={2} sx={{ p: 1, minWidth: 200 }}>
+            <Stack spacing={2} sx={{ p: 1, width: '100%' }}>
               <TextField
-                label="Min Age"
-                type="number"
+                label="Surname"
                 size="small"
-                value={minAge}
-                onChange={(e) => setMinAge(e.target.value)}
+                value={surname}
+                onChange={(e) => setSurname(e.target.value)}
               />
               <TextField
-                label="Max Age"
+                label="Forename"
+                size="small"
+                value={forename}
+                onChange={(e) => setForename(e.target.value)}
+              />
+              <TextField
+                label="Age (+/- 5 years)"
                 type="number"
                 size="small"
-                value={maxAge}
-                onChange={(e) => setMaxAge(e.target.value)}
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
               />
+              <TextField
+                label="County"
+                size="small"
+                value="Longford"
+                disabled
+              />
+              
+              {showMoreFilters && (
+                <>
+                  <FormControl size="small" fullWidth>
+                    <InputLabel>Sex</InputLabel>
+                    <Select
+                      value={sex}
+                      label="Sex"
+                      onChange={(e) => setSex(e.target.value)}
+                    >
+                      <MenuItem value="">All</MenuItem>
+                      <MenuItem value="Male">Male</MenuItem>
+                      <MenuItem value="Female">Female</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <TextField
+                    label="Occupation"
+                    size="small"
+                    value={occupation}
+                    onChange={(e) => setOccupation(e.target.value)}
+                  />
+
+                  <FormControl size="small" fullWidth>
+                    <InputLabel>County/Country of Origin</InputLabel>
+                    <Select
+                      value={countyOrigin}
+                      label="County/Country of Origin"
+                      onChange={(e) => setCountyOrigin(e.target.value)}
+                    >
+                      <MenuItem value="">All</MenuItem>
+                      <MenuItem value="Longford">Longford</MenuItem>
+                      <MenuItem value="Dublin">Dublin</MenuItem>
+                      <MenuItem value="Cork">Cork</MenuItem>
+                      <MenuItem value="Galway">Galway</MenuItem>
+                      <MenuItem value="Mayo">Mayo</MenuItem>
+                      <MenuItem value="England">England</MenuItem>
+                      <MenuItem value="Scotland">Scotland</MenuItem>
+                      <MenuItem value="America">America</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <FormControl size="small" fullWidth>
+                    <InputLabel>Religion</InputLabel>
+                    <Select
+                      value={religion}
+                      label="Religion"
+                      onChange={(e) => setReligion(e.target.value)}
+                    >
+                      <MenuItem value="">All</MenuItem>
+                      <MenuItem value="Roman Catholic">Roman Catholic</MenuItem>
+                      <MenuItem value="Church of Ireland">Church of Ireland</MenuItem>
+                      <MenuItem value="Presbyterian">Presbyterian</MenuItem>
+                      <MenuItem value="Methodist">Methodist</MenuItem>
+                      <MenuItem value="Baptist">Baptist</MenuItem>
+                      <MenuItem value="Other">Other</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <FormControl size="small" fullWidth>
+                    <InputLabel>Literacy</InputLabel>
+                    <Select
+                      value={literacy}
+                      label="Literacy"
+                      onChange={(e) => setLiteracy(e.target.value)}
+                    >
+                      <MenuItem value="">All</MenuItem>
+                      <MenuItem value="Read and Write">Read and Write</MenuItem>
+                      <MenuItem value="Read Only">Read Only</MenuItem>
+                      <MenuItem value="Cannot Read">Cannot Read</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <FormControl size="small" fullWidth>
+                    <InputLabel>Marital Status</InputLabel>
+                    <Select
+                      value={maritalStatus}
+                      label="Marital Status"
+                      onChange={(e) => setMaritalStatus(e.target.value)}
+                    >
+                      <MenuItem value="">All</MenuItem>
+                      <MenuItem value="Single">Single</MenuItem>
+                      <MenuItem value="Married">Married</MenuItem>
+                      <MenuItem value="Widowed">Widowed</MenuItem>
+                      <MenuItem value="Divorced">Divorced</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <FormControl size="small" fullWidth>
+                    <InputLabel>Relationship to Head</InputLabel>
+                    <Select
+                      value={relationship}
+                      label="Relationship to Head"
+                      onChange={(e) => setRelationship(e.target.value)}
+                    >
+                      <MenuItem value="">All</MenuItem>
+                      <MenuItem value="Head">Head</MenuItem>
+                      <MenuItem value="Wife">Wife</MenuItem>
+                      <MenuItem value="Son">Son</MenuItem>
+                      <MenuItem value="Daughter">Daughter</MenuItem>
+                      <MenuItem value="Father">Father</MenuItem>
+                      <MenuItem value="Mother">Mother</MenuItem>
+                      <MenuItem value="Brother">Brother</MenuItem>
+                      <MenuItem value="Sister">Sister</MenuItem>
+                      <MenuItem value="Servant">Servant</MenuItem>
+                      <MenuItem value="Boarder">Boarder</MenuItem>
+                      <MenuItem value="Visitor">Visitor</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <TextField
+                    label="Language Proficiency"
+                    size="small"
+                    value="Irish and English"
+                    disabled
+                  />
+                  <FormControl size="small" fullWidth>
+                    <InputLabel>Specified Illnesses</InputLabel>
+                    <Select
+                      value={specifiedIllnesses}
+                      label="Specified Illnesses"
+                      onChange={(e) => setSpecifiedIllnesses(e.target.value)}
+                    >
+                      <MenuItem value="">All</MenuItem>
+                      <MenuItem value="None">None</MenuItem>
+                      <MenuItem value="Blind">Blind</MenuItem>
+                      <MenuItem value="Deaf and Dumb">Deaf and Dumb</MenuItem>
+                      <MenuItem value="Imbecile">Imbecile</MenuItem>
+                      <MenuItem value="Lunatic">Lunatic</MenuItem>
+                      <MenuItem value="Other">Other</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <TextField
+                    label="Number of Years Married"
+                    type="number"
+                    size="small"
+                    value={yearsMarried}
+                    onChange={(e) => setYearsMarried(e.target.value)}
+                  />
+                  <TextField
+                    label="Number of Children Born"
+                    type="number"
+                    size="small"
+                    value={childrenBorn}
+                    onChange={(e) => setChildrenBorn(e.target.value)}
+                  />
+                  <TextField
+                    label="Number of Children Living"
+                    type="number"
+                    size="small"
+                    value={childrenLiving}
+                    onChange={(e) => setChildrenLiving(e.target.value)}
+                  />
+
+                </>
+              )}
+              
+              <Button
+                variant="text"
+                size="small"
+                onClick={() => setShowMoreFilters(!showMoreFilters)}
+              >
+                {showMoreFilters ? 'Show Less Filters' : 'Show More Filters'}
+              </Button>
             </Stack>
           </MenuItem>
         </Menu>
